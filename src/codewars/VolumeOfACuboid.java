b9aa7d8d78c52ec6e74c9c3cb3397b92205425ee
Operans <chingiz.kartanbaev@gmail.com>
2023-11-25 15:39:59 +0600

    dayFour

---
+++ src/codewars/VolumeOfACuboid.java
@@ -0,0 +1,9 @@
+package codewars;
+
+public class VolumeOfACuboid {
+
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length * width * height;
+    }
+
+}

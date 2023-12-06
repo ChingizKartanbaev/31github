3d413897d713f109f39c2f9b51fe971fb382ef7a
Operans <chingiz.kartanbaev@gmail.com>
2023-12-06 20:48:00 +0600

    dayFifteen

--- src/codewars/HowGoodAreYouReally.java
+++ src/codewars/HowGoodAreYouReally.java
@@ -2,14 +2,8 @@ package codewars;
 
 public class HowGoodAreYouReally {
 
-    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
-        int res = 0;
-        for (int el : classPoints) {
-            res += el;
-        }
-
-        return res / classPoints.length < yourPoints;
+    public static boolean isPlural(float f){
+        return f != 1 ;
     }
-
 }
 

7ef10030eea7545b54e1dc596c7f833ac06fc666
Operans <chingiz.kartanbaev@gmail.com>
2023-12-11 21:58:10 +0600

    DayTwenty

---
+++ src/codewars/NameMe.java
@@ -0,0 +1,24 @@
+public class NameMe {
+    public String firstName;
+    public String lastName;
+    public String fullName;
+
+    public NameMe(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+        this.fullName = first + " " + last;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+}

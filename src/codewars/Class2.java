c78fc8127aac226b5324e5c093c4b1bd1b3e9875
Operans <chingiz.kartanbaev@gmail.com>
2023-12-19 22:16:18 +0600

    DayTwentyFive

--- src/codewars/Class2.java
+++ src/codewars/Class2.java
@@ -1,4 +1,19 @@
 package codewars;
 
 public class Class2 {
+
+
+    public static String datingRange(int age) {
+        int min = 0, max = 0;
+
+        if(age <= 14) {
+            min = (int) (age - 0.10 * age);
+            max = (int) (age + 0.10 * age);
+        } else {
+            min = age / 2 + 7;
+            max = (age - 7) * 2;
+        }
+
+        return min  + "-" + max;
+    }
 }

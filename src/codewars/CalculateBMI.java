3b1a6b8f5733a82413f06c03dd9f1581047a8874
Operans <chingiz.kartanbaev@gmail.com>
2023-12-24 22:13:32 +0600

    DayTwentyTen

---
+++ src/codewars/CalculateBMI.java
@@ -0,0 +1,18 @@
+package codewars;
+
+public class CalculateBMI {
+
+    public static String bmi(double weight, double height) {
+        double bmi = weight / Math.pow(height, 2);
+        if(bmi <= 18.5) {
+            return "Underweight";
+        } else if (bmi <= 25.0) {
+            return "Normal";
+        } else if (bmi <= 30.0) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+
+}
